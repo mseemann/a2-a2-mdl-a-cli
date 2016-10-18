3bf67383cb46da45ed6be334ee6066bfe9c9d5c6
Michael Seemann <seemann@mseemann.de>
2016-10-18 09:01:11 +0200

    add demo code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MdlDialogService } from 'angular2-mdl';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private dialogService: MdlDialogService){}
+
+  public test(){
+   this.dialogService.alert("test");
+  }
 }
